79f883cac126bd981cdcd1ca85667f169a141678
Yevgeny Trachtinov <yevgenytr@cloudzone.io>
2018-07-21 19:49:04 +0300

    Shorter lines for more easy read

--- create-compute.sh
+++ create-compute.sh
@@ -14,9 +14,11 @@ done
 for (( i=1; i<=${COUNT}; i++ ))
   do
     gcloud beta compute --project=$GCP_PROJECT instances create ${ENV}-glusterfs-node-${i} \
-    --zone=${GCP_REGION}-${GCP_AZS[$i-1]} --machine-type=$GFS_NODE_TYPE --subnet=$GCP_SUBNET --network-tier=PREMIUM --maintenance-policy=MIGRATE \
-    --service-account=$GCP_SVC_ACC --scopes=$GCP_API_SCOPE --tags=$GCP_FW_TAG\
-    --image=$GCP_IMAGE --image-project=$GCP_IMAGE_PRJ --boot-disk-size=$GCP_BOOT_DISK_SIZE --boot-disk-type=$GCP_BOOT_DISK_TYPE \
+    --zone=${GCP_REGION}-${GCP_AZS[$i-1]} --machine-type=$GFS_NODE_TYPE --subnet=$GCP_SUBNET \
+    --network-tier=PREMIUM --maintenance-policy=MIGRATE \
+    --service-account=$GCP_SVC_ACC --scopes=$GCP_API_SCOPE --tags=$GCP_FW_TAG \
+    --image=$GCP_IMAGE --image-project=$GCP_IMAGE_PRJ --boot-disk-size=$GCP_BOOT_DISK_SIZE \
+    --boot-disk-type=$GCP_BOOT_DISK_TYPE \
     --boot-disk-device-name=glusterfs-node-${i} --disk=name=${ENV}-gfs-data-disk-${i},device-name=gfs-data-disk,mode=rw,boot=no \
     --metadata-from-file startup-script=$BOOSTRAP_SCRIPT \
     --verbosity=$GCLOUD_VERBOSITY
